8a61aca27593c468b863ae3b43abcb1694b1fbc0
Ryan <rdorrity@gmail.com>
2019-01-27 23:18:48 -0800

    web app initialized
    Created back end of web app. html, py, httpserver files all started. Currently can add people to the staff database. Will continue to add support for other tables.

---
+++ web_app/cgi-bin/staff_db.py
@@ -0,0 +1,57 @@
+# login.py
+# this file must be in the /cgi-bin/ directory of the server
+import cgitb , cgi
+import mysql.connector
+cgitb.enable()
+form = cgi.FieldStorage()
+#
+#  code to get input values goes here
+#
+first = form['first'].value 
+last = form['last'].value
+view_sd = False
+add = False
+if "view_sd" in form:
+    view_sd=True 
+elif "add" in form:
+    add=True 
+print("Content-Type: text/html")    # HTML is following
+print()                             # blank line required, end of headers
+print("<html><body>")
+print("<p>", first, last, "was added to the database.</p><br>")
+qsql = 'SELECT * FROM staff'  
+insert_sql = 'INSERT INTO staff (first_name, last_name) VALUES (%s, %s)'
+update_sql = 'UPDATE login SET visits = visits + 1 WHERE first=%s and last=%s'
+
+# connect to database
+cnx = mysql.connector.connect(user='root',
+                                password='sesame',
+                                database='sd',
+                                host='127.0.0.1')
+
+ 
+#  code to do SQL goes here
+#cursor = cnx.cursor()
+#cursor.execute(qsql, (first, last))
+#row = cursor.fetchone()
+
+
+if add is True:	
+	cursorb = cnx.cursor()
+	cursorb.execute(insert_sql, (first, last))
+	#visit_number = row[0] + 1				
+	print('Click')
+	#<a href="http://127.0.0.1:8000/Staff_db.html" target="_self">here</a>
+		
+if view_sd is True:	
+ 
+	# must be first visit, insert row
+	cursorb = cnx.cursor()
+	cursorb.execute(qsql)	
+	#print('Thank you for registering.  Come visit again.')
+
+	# 
+	#print('first already taken. Please select a new userid.')
+print("</body></html>")
+cnx.commit()
+cnx.close()  # close the connection 
