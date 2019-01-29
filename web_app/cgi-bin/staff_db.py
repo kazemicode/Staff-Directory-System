0d39b8a04aa626d292ff4fa0048734590353e85f
Ryan <rdorrity@gmail.com>
2019-01-29 09:43:25 -0800

    Update staff_db.py

--- web_app/cgi-bin/staff_db.py
+++ web_app/cgi-bin/staff_db.py
@@ -43,7 +43,36 @@ if add is True:
 	print('Click <a href="http://127.0.0.1:8000/Staff_db.html" target="_self">here</a>')
 		
 if view_sd is True:	
-	
+	print('<?php\
+	try { \
+	$con= new PDO("mysql:host=localhost;dbname=sd", "root", "sesame");\
+	$con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);\
+	$query = "SELECT * FROM staff";\
+	//first pass just gets the column names\
+	print "<table> n";\
+	$result = $con->query($query);\
+	//return only the first row (we only need field names)\
+	$row = $result->fetch(PDO::FETCH_ASSOC);\
+	print " <tr> n";\
+	foreach ($row as $field => $value){\
+	print " <th>$field</th> n";\
+	} // end foreach\
+	print " </tr> n";\
+	//second query gets the data\
+	$data = $con->query($query);\
+	$data->setFetchMode(PDO::FETCH_ASSOC);\
+	foreach($data as $row){\
+	print " <tr> n";\
+	foreach ($row as $name=>$value){\
+	print " <td>$value</td> n";\
+	} // end field loop\
+	print " </tr> n";\
+	} // end record loop\
+	print "</table> n";\
+	} catch(PDOException $e) {\
+	echo "ERROR: " . $e->getMessage();\
+	} // end try\
+	?>')
 	# must be first visit, insert row
 	#cursorb = cnx.cursor()
 	#cursorb.execute(qsql)	
