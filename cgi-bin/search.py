469950a8456b9ce912d3df2d32df0d7e6ab0b43a
kazemicode <sarieses@gmail.com>
2019-01-28 20:18:08 -0800

    Update search.py

--- cgi-bin/search.py
+++ cgi-bin/search.py
@@ -13,11 +13,7 @@ cgitb.enable()
 form = cgi.FieldStorage()  # Object that allows retrieval of input from form
 
 if 'last_name' not in form:
-    print("Content-Type: text/html")  # HTML is following
-    print()  # blank line required, end of headers
-    print("<html><head><meta http-equiv=\"Refresh\" content=\"7; url=/staff.html\"/></head>")
-    print("<body><p>You must enter a last name. <a href=\"/staff.html\">Return to search</a> </p></body></html>")
-    sys.exit()
+    last_name = None
 else:
     last_name = form['last_name'].value  # get the value assigned to the userid attribute in form
 
@@ -49,7 +45,8 @@ dept_query = 'SELECT last_name, first_name, period, course_title, room_number\
                         ON s.staff_id = sch.staff_id\
                     JOIN courses c\
                         ON sch.course_id = c.course_id\
-                    WHERE (department_id = %s OR department_id = 11)'
+                    WHERE (department_id = %s)\
+                    ORDER BY last_name, first_name, period'
 
 nofirst_query = 'SELECT last_name, first_name, period, course_title, room_number\
                     FROM staff s\
@@ -59,6 +56,16 @@ nofirst_query = 'SELECT last_name, first_name, period, course_title, room_number
                         ON sch.course_id = c.course_id\
                     WHERE last_name = %s\
                     AND (department_id = %s OR department_id = 11)'
+
+
+nofirst_ordept_query = 'SELECT last_name, first_name, period, course_title, room_number\
+                    FROM staff s\
+                    JOIN schedules sch\
+                        ON s.staff_id = sch.staff_id\
+                    JOIN courses c\
+                        ON sch.course_id = c.course_id\
+                    WHERE last_name = %s\
+                    ORDER BY department_id, last_name, first_name, period'
 
 # connect to database
 cnx = mysql.connector.connect(user='root',
@@ -78,18 +85,35 @@ if first_name != None and last_name != None and department != 0:
     rows = cursor.fetchall()  # fetches the result of the SELECT clause
 
 
-    if rows is None:
-        print('<p>No staff members match your query.</p>', last_name, first_name)
-    else:
-        print("<table><tr><th>Last Name</th><th>First Name</th><th>Period</th><th>Course Title</th>\
+elif last_name != None and department != 0:
+    cursor.execute(nofirst_query, (last_name, department))
+    rows = cursor.fetchall()  # fetches the result of the SELECT clause
+
+
+elif department != 0:
+    cursor.execute(dept_query, (department, ))
+    rows = cursor.fetchall()  # fetches the result of the SELECT clause
+
+else:
+    cursor.execute(nofirst_ordept_query, (department, ))
+    rows = cursor.fetchall()  # fetches the result of the SELECT clause
+
+if len(rows) == 0:
+    print("<html><head><meta http-equiv=\"Refresh\" content=\"7; url=/staff.html\"/></head>")
+    print("<body><p>No results found.. <a href=\"/staff.html\">Return to search</a> </p></body></html>")
+    sys.exit()
+else:
+    print("<table><tr><th>Last Name</th><th>First Name</th><th>Period</th><th>Course Title</th>\
                 <th>Room</td></tr>")
-        for row in rows:
-            print("<tr>")
-            for column in row:
-                print("<td>",column,"</td>")
-            print("</tr>")
-    print("</table>")
-    print("</body></html>")
+    for row in rows:
+        print("<tr>")
+        for column in row:
+            print("<td>", column, "</td>")
+        print("</tr>")
+print("</table>")
+print("<a href=\"/staff.html\">Return to search</a></body></html>")
+
+
 
 #cnx.commit()    # IMPORTANT. Must commit or else the inserts and updates will not be permanent
 cnx.close()     # close the connection
