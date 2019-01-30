26a601845fb94924f5b32f5ab0c54b297d976021
kazemicode <sarieses@gmail.com>
2019-01-29 16:19:27 -0800

    added more rows

--- cgi-bin/search.py
+++ cgi-bin/search.py
@@ -12,22 +12,24 @@ import sys
 cgitb.enable()
 form = cgi.FieldStorage()  # Object that allows retrieval of input from form
 
+
+# Check to see if what fields were filled out by the user. Assign None left blank
 if 'last_name' not in form:
     last_name = None
 else:
-    last_name = form['last_name'].value  # get the value assigned to the userid attribute in form
+    last_name = form['last_name'].value  # get the value assigned to the last_name attribute in form
 
 if 'first_name' not in form:
     first_name = None
 else:
-    first_name = form['first_name'].value  # get the value assigned to the password attribute in form
+    first_name = form['first_name'].value  # get the value assigned to the first_name attribute in form
 
-department = form['department'].value
-
-
+department = form['department'].value # get the value assigned to the department attribute in the field
 
 
 
+
+# This query is triggered if all fields are filled out
 qsql = 'SELECT last_name, first_name, period, course_title, room_number\
                     FROM staff s\
                     JOIN schedules sch\
@@ -39,7 +41,7 @@ qsql = 'SELECT last_name, first_name, period, course_title, room_number\
                     AND (department_id = %s OR department_id = 11)'
 
 
-
+# This query is triggered if only a department is supplied
 dept_query = 'SELECT last_name, first_name, period, course_title, room_number\
                     FROM staff s\
                     JOIN schedules sch\
@@ -49,6 +51,7 @@ dept_query = 'SELECT last_name, first_name, period, course_title, room_number\
                     WHERE (department_id = %s)\
                     ORDER BY last_name, first_name, period'
 
+# This query is triggered if only the last name and department are supplied
 nofirst_query = 'SELECT last_name, first_name, period, course_title, room_number\
                     FROM staff s\
                     JOIN schedules sch\
@@ -59,7 +62,7 @@ nofirst_query = 'SELECT last_name, first_name, period, course_title, room_number
                     AND (department_id = %s OR department_id = 11)\
                     ORDER BY last_name, first_name, period'
 
-
+# This query is triggered if only the last name is supplied
 nofirst_ordept_query = 'SELECT last_name, first_name, period, course_title, room_number\
                     FROM staff s\
                     JOIN schedules sch\
@@ -78,10 +81,15 @@ cnx = mysql.connector.connect(user='root',
 
 #  code to do SQL goes here
 cursor = cnx.cursor()  # Creates a cursor object, which is used to execute a MySQL query
+
+
+# Beginning HTML for dynamically created results page
 print("Content-Type: text/html")  # HTML is following
 print()  # blank line required, end of headers
 print("<html><head><style> table, th, td { border: 1px solid black; padding: 15px;} </style></head><body>")
 
+
+# Determines which query is triggered
 if first_name != None and last_name != None and department != "0":
     #print('all fields filled')
     cursor.execute(qsql, (last_name, first_name, department))
@@ -103,16 +111,20 @@ elif last_name != None and department != "0":
     cursor.execute(nofirst_query, (last_name, department))
     rows = cursor.fetchall()  # fetches the result of the SELECT clause
 
+# If no query is triggered, it means that user did not give sufficient info. Prompt to try again
 else:
     print("<html><head><meta http-equiv=\"Refresh\" content=\"7; url=/staff.html\"/></head>")
     print("<body><p>Please supply a last name or department. <a href=\"/staff.html\">Return to search</a> </p></body></html>")
     sys.exit()
 
 
+# Checks to see if we got results, if not, inform user to try again
 if len(rows) == 0:
     print("<html><head><meta http-equiv=\"Refresh\" content=\"7; url=/staff.html\"/></head>")
     print("<body><p>No results found. <a href=\"/staff.html\">Return to search</a> </p></body></html>")
     sys.exit()
+
+# Otherwise, show results in a nice table
 else:
     print("<table><tr><th>Last Name</th><th>First Name</th><th>Period</th><th>Course Title</th>\
                 <th>Room</td></tr>")
@@ -125,6 +137,6 @@ print("</table>")
 print("<a href=\"/staff.html\">Return to search</a></body></html>")
 
 
-
+# line below not needed since we do not alter the table
 #cnx.commit()    # IMPORTANT. Must commit or else the inserts and updates will not be permanent
 cnx.close()     # close the connection
--- staff_directory_setup_sql.sql
+++ staff_directory_setup_sql.sql
@@ -76,7 +76,12 @@ INSERT INTO staff VALUES
 (3, "Cynthia", "Andreson"),
 (4, "Alsacia", "Apodaca"),
 (5, "Craig", "Balsley"),
-(6, "Alicia", "Bena");
+(6, "Alicia", "Bena"),
+(7, "Janel", "Bruan"),
+(8, "Krista", "Burnet"),
+(9, "Alexis", "Camacho"),
+(10, "Sara", "Kazemi"),
+(11, "Jonathan", "Chase");
 
 INSERT INTO departments VALUES 
 (1, "Computer Science"),
@@ -104,7 +109,21 @@ INSERT INTO courses VALUES
 (9, "Integrated Math III", 4),
 (10, "PE Course I", 5),
 (11, "PE Course II", 5),
-(12, "AP United States History", 7);
+(12, "AP United States History", 7),
+(13, "ELD IV", 3),
+(14, "ELD III", 3),
+(15, "English 9", 3),
+(16, "Literacy Workshop 9", 3),
+(17, "Reading Moderate", 8),
+(18, "Social Science Moderate", 8),
+(19, "Comprehensive Health Moderate", 8),
+(20, "English 10", 3),
+(21, "AP Computer Science A", 1),
+(22, "AP Computer Science Principles", 1),
+(23, "Exploring Computer Science", 1),
+(24, "Chemistry", 6),
+(25, "AP Chemistry", 6),
+(26, "Chemistry Honors", 6);
 
 INSERT INTO schedules VALUES
 -- schedule_id, period, room_number, staff_id, course_id)
@@ -143,7 +162,37 @@ INSERT INTO schedules VALUES
 (33, 3, 153, 6, 5),
 (34, 4, 153, 6, 9),
 (35, 5, 153, 6, 7),
-(36, 6, 153, 6, 9);
+(36, 6, 153, 6, 9),
+(37, 1, 906, 7, 5),
+(38, 2, 906, 7, 13),
+(39, 3, 906, 7, 14),
+(40, 4, 906, 7, 14),
+(41, 5, 906, 7, 14),
+(42, 6, 906, 7, 14),
+(43, 1, 312, 8, 15),
+(44, 2, 312, 8, 15),
+(45, 3, 312, 8, 16),
+(46, 4, 312, 8, 15),
+(47, 5, 312, 8, 15),
+(48, 6, 312, 8, 5),
+(49, 1, 711, 9, 17),
+(50, 2, 711, 9, 17),
+(51, 3, 307, 9, 20),
+(52, 4, 711, 9, 18),
+(53, 5, 711, 9, 18),
+(54, 6, 711, 9, 19),
+(55, 1, 214, 10, 5),
+(56, 2, 214, 10, 22),
+(57, 3, 214, 10, 22),
+(58, 4, 214, 10, 21),
+(59, 5, 214, 10, 23),
+(60, 6, 214, 10, 21),
+(61, 1, 1307, 11, 24),
+(62, 2, 1307, 11, 25),
+(63, 3, 1307, 11, 26),
+(64, 4, 1307, 11, 26),
+(65, 5, 1307, 11, 26),
+(66, 6, 1307, 11, 26);
 
 INSERT INTO department_membership VALUES
 (1, 1, 7),
@@ -152,9 +201,12 @@ INSERT INTO department_membership VALUES
 (4, 3, 11),
 (5, 4, 4),
 (6, 5, 5),
-(7, 6, 4);
-
-
-
+(7, 6, 4),
+(8, 7, 3),
+(9, 8, 3),
+(10, 9, 3),
+(11, 9, 8),
+(12, 10, 1),
+(13, 11, 6);
 
 
